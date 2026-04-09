b3874d34271f25d5d6cb8766fa9c2a9158ebca46
praseedhamaddipatla <praseedha.maddipatla@gmail.com>
2026-04-08 23:02:36 -0500

    crashing out

--- hdl/fetch.sv
+++ hdl/fetch.sv
@@ -33,7 +33,11 @@ module fetch (
   assign pc = pc_reg;
 
   // branch is taken if ALU says so (brnz/brgt) or if unconditional jump
-  wire taken = (is_branch && branch_cond) || is_jump;
+  wire taken =
+    (is_branch && branch_cond) ||
+    is_jump ||
+    is_call ||
+    is_return;
 
   //mux
   wire [63:0] next_pc =
